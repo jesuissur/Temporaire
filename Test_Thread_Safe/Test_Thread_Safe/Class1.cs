fccf3397e0bb53de00b1222d600b2898fb95ff68
Phil <philippe.tremblay@elapsetech.com>
2013-03-11 15:51:12 -0400

    Exemple de Test unitaire thread safe

---
+++ Test_Thread_Safe/Test_Thread_Safe/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Test_Thread_Safe
+{
+    public static class Class1
+    {
+
+        private static MySelf _mySelf;
+        private static readonly Object _lockObject = new object();
+
+        public static MySelf Instance
+        {
+            get { return _mySelf ?? (_mySelf = new MySelf()); } // NON Thread safe
+            //get { return ThreadTools.AssignWhenNotInitialized(_lockObject, ref _mySelf, () => new MySelf()); } // Thread safe
+        }
+    }
+
+    public static class ThreadTools
+    {
+        public static T AssignWhenNotInitialized<T>(object lockObject, ref T instance, Func<T> createNew) where T:class 
+        {
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = createNew();
+                        Console.WriteLine("NEW");
+                    }
+                }
+            }
+            return instance;
+        }
+
+    }
+
+    public class MySelf
+    {
+        public string SomePublicField;
+    }
+
+    [TestFixture]
+    public class Class1Test
+    {
+        [Test]
+        public void IsItThreadSafe()
+        {
+            var threadList = new List<Thread>();
+            for (int i = 0; i < 300; i++)
+            {
+                var thread = new Thread(DoSomeNotThreadSafeOperation);
+                threadList.Add(thread);
+            }
+            threadList.ForEach(x => x.Start());
+            threadList.ForEach(x => x.Join());
+            
+        }
+
+        private void DoSomeNotThreadSafeOperation()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                var previousInstance = Class1.Instance;
+                previousInstance.Should().Be(Class1.Instance);
+            }
+        }
+    }
+
+}
---
+++ Test_Thread_Safe/Test_Thread_Safe/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test_Thread_Safe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test_Thread_Safe")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f96a7810-f055-445c-b6a4-b6bd1b3fb029")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
