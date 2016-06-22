297d416a1a9e4c2bfbd188c9971cb5d3e8e66401
Philippe Tremblay <Philippe.Tremblay@ElapseTech.com>
2016-06-21 20:57:02 -0400

    First push for MediatR test project

---
+++ Messaging.InProcess/Messaging.InProcess/FabriqueInjectionDependancesTest.vb
@@ -0,0 +1,29 @@
+﻿Imports FluentAssertions
+Imports MediatR
+Imports Microsoft.Practices.Unity
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class FabriqueInjectionDependancesTest
+
+    <TestMethod()>
+    Public sub ObtenirInstance_Devrait_Configurer_Mediator()
+        dim sujet = new FabriqueInjectionDependances()
+
+        Dim Injecteur as IUnityContainer = sujet.ObtenirInstance()
+
+        Injecteur.Should().NotBeNull()
+
+        Injecteur.Resolve(Of IMediator)().Should().NotBeNull()
+    End sub 
+End Class
+
+Public Class FabriqueInjectionDependances
+    Public Function ObtenirInstance() As IUnityContainer
+        Dim injecteur = new UnityContainer()
+        injecteur.RegisterType(Of IMediator, Mediator)
+        injecteur.RegisterInstance(Of SingleInstanceFactory)(Function(t) injecteur.Resolve(t))
+        injecteur.RegisterInstance(Of MultiInstanceFactory)(Function(t) injecteur.ResolveAll(t))
+        return injecteur
+    End Function
+End Class
